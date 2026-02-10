44670af627979eb7ae9ae6292d60d3088f3c193f
Francesco020 <francescobucci2002@gmail.com>
2026-02-10 04:25:28 +0100

    Add function encrypt_file using Fernet in encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -1,6 +1,7 @@
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
+from cryptography.fernet import Fernet
 import base64
 
 def derive_key(password: str, salt: bytes): 
@@ -13,3 +14,17 @@ def derive_key(password: str, salt: bytes):
     )
     return base64.urlsafe_b64encode(kdf.derive(password.encode()))
 
+def encrypt_file(input_file: str, key: bytes):
+    with open(input_file, "rb") as f:
+        data = f.read()
+        
+    fernet = Fernet(key)
+    
+    encrypted_data = fernet.encrypt(data)
+    
+    output_file = input_file + ".enc"
+    with open(output_file, "wb") as f:
+        f.write(encrypted_data)
+        
+    print("File encrypted successfully: {output_file}")
+    
